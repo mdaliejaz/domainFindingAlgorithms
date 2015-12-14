868b88a1fae1bdc00197cd303484b20bb0564146
Ali <mdaliejaz@gmail.com>
2015-12-14 18:06:48 -0500

    Added check for early failure and refactored code.

--- HiCseg/src/HiCseg_linkR.c
+++ HiCseg/src/HiCseg_linkR.c
@@ -10,35 +10,47 @@
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_sf_gamma.h>
 
-int Function_HiC_R(int *size, int *K, char** distrib, double* matrix, int* tchap, double* Jvect, int* t_est_mat, char** modele)
+int Function_HiC_R(int *size, int *maximum_no_change_points, char **distribution,
+	double *matrix, int *tchap, double *Jvect, int *t_est_mat, char **modele)
 {
+	/*
+	 * R sends pointers of each data, keeping it that way for now.
+	 * This section can be removed if parameters received by
+	 * this method is changed.
+	 */
 	int size_matrix = *size;
-	int Kmax = *K;
-	char *distribution = *distrib;
+	int max_change_points = *maximum_no_change_points;
+	char *distrib = *distribution;
 	char *model = *modele;
+	/*
+	 * End sanitizing input data
+	 */
 
 	int i, j, k, l, size_mat_extr, i_coin, j_coin, size_coin, n_coin, size_mu;
-	double somme, loglambdahat, somme_carr;
+	int ind_max, u;
+	double sum, loglambdahat, sum_carr;
 	double max, mu_hat, var_hat, phi_hat, lambda_hat, mu;
-	int ind_max;
-	int u;
-	
 	double vecteur[size_matrix - 1];
-	double t_est[Kmax][Kmax];
-	double y[size_matrix][size_matrix], Delta[size_matrix][size_matrix];
-	double Exterieur[size_matrix][size_matrix], T[size_matrix][size_matrix], D[size_matrix][size_matrix];
-	double R[size_matrix][size_matrix], Tcarr[size_matrix][size_matrix], Dcarr[size_matrix][size_matrix], Rcarr[size_matrix][size_matrix];
-	double I[Kmax][size_matrix], t_matrix[Kmax - 1][size_matrix];
+	double t_est[max_change_points][max_change_points], y[size_matrix][size_matrix];
+	double Delta[size_matrix][size_matrix], Exterieur[size_matrix][size_matrix];
+	double T[size_matrix][size_matrix], D[size_matrix][size_matrix];
+	double R[size_matrix][size_matrix], Tcarr[size_matrix][size_matrix];
+	double Dcarr[size_matrix][size_matrix], Rcarr[size_matrix][size_matrix];
+	double I[max_change_points][size_matrix], t_matrix[max_change_points - 1][size_matrix];
 
 
-	// exit if model is neither D nor Dplus
+	// Early exit for wrong parameters
 	if ((strcmp(model, "D") != 0) && (strcmp(model, "Dplus") != 0))
+		return EXIT_FAILURE;
+
+	if ((strcmp(distrib, "P") != 0) && (strcmp(distrib, "B") != 0) &&
+		(strcmp(distrib, "G") != 0))
 		return EXIT_FAILURE;
 
 	// initialize all the matrices
 	for (i = 0; i < size_matrix; i++) {
 		for (j = 0; j < size_matrix; j++) {
-
+			// Copying input data matrix to matrix Y
 			y[i][j] = matrix[i * size_matrix + j];
 			// 1E100 = scientific notation for vary large number.
 			// -1E100 = (-) 1 followed by 100 zeroes
@@ -63,17 +75,17 @@ int Function_HiC_R(int *size, int *K, char** distrib, double* matrix, int* tchap
 	for (k = 1; k < size_matrix; k++) {
 		T[0][k] = T[0][(k - 1)] + y[0][k];
 		D[k][k] = y[k][k];
-		somme = 0;
+		sum = 0;
 		for (i = 0; i <= k; i++) {
-			somme = somme + y[i][k];
+			sum = sum + y[i][k];
 		}
-		T[k][k] = T[(k - 1)][(k - 1)] + somme;
+		T[k][k] = T[(k - 1)][(k - 1)] + sum;
 		D[0][k] = T[k][k];
 	}
 
 
 	// Poisson Distribution
-	if (strcmp(distribution, "P") == 0) {
+	if (strcmp(distrib, "P") == 0) {
 		/////  Calculating amounts in trapezes
 		if (D[0][0] != 0)
 			Delta[0][0] = D[0][0] * (log(D[0][0]) - 1);
@@ -88,9 +100,9 @@ int Function_HiC_R(int *size, int *K, char** distrib, double* matrix, int* tchap
 		if (strcmp(model, "Dplus") == 0) {
 			for (i = 1; i < (size_matrix - 1); i++) {
 				for (j = (i + 1); j < size_matrix; j++) {
-					somme = 0;
-					for (k = 0; k <= i; k++) somme = somme + y[k][j];
-					T[i][j] = T[i][(j - 1)] + somme;
+					sum = 0;
+					for (k = 0; k <= i; k++) sum = sum + y[k][j];
+					T[i][j] = T[i][(j - 1)] + sum;
 					R[i][j] = T[(i - 1)][j] - T[(i - 1)][(i - 1)];
 					D[i][j] = T[j][j] - T[(i - 1)][j];
 					size_mat_extr = (j - i + 1) * (j - i) / 2 + (j - i + 1);
@@ -112,23 +124,23 @@ int Function_HiC_R(int *size, int *K, char** distrib, double* matrix, int* tchap
 			size_coin = (n_coin + 1) * n_coin / 2;
 			j_coin = 3 * n_coin;
 			i_coin = n_coin;
-			somme = 0.;
+			sum = 0;
 
 			for (i = 0; i < i_coin; i++) {
 				for (j = j_coin; j < size_matrix; j++) {
 					if ((j - j_coin) >= i) {
-						somme = somme + y[i][j];
+						sum = sum + y[i][j];
 					}
 				}
 			}
-			lambda_hat = somme / size_coin;
+			lambda_hat = sum / size_coin;
 			loglambdahat = log(lambda_hat);
 
 			for (i = 1; i < (size_matrix - 1); i++) {
 				for (j = (i + 1); j < size_matrix; j++) {
-					somme = 0;
-					for (k = 0; k <= i; k++) somme = somme + y[k][j];
-					T[i][j] = T[i][(j - 1)] + somme;
+					sum = 0;
+					for (k = 0; k <= i; k++) sum = sum + y[k][j];
+					T[i][j] = T[i][(j - 1)] + sum;
 					R[i][j] = T[(i - 1)][j] - T[(i - 1)][(i - 1)];
 					D[i][j] = T[j][j] - T[(i - 1)][j];
 					size_mat_extr = (j - i + 1) * (j - i) / 2 + (j - i + 1);
@@ -140,7 +152,7 @@ int Function_HiC_R(int *size, int *K, char** distrib, double* matrix, int* tchap
 				}
 			}
 		}
-	} else if (strcmp(distribution, "B") == 0) {
+	} else if (strcmp(distrib, "B") == 0) {
 		///// Calculating phi_hat and mu_hat
 
 		n_coin = (int)floor(size_matrix / 4);
@@ -148,140 +160,119 @@ int Function_HiC_R(int *size, int *K, char** distrib, double* matrix, int* tchap
 		size_coin = (n_coin + 1) * n_coin / 2;
 		j_coin = 3 * n_coin;
 		i_coin = n_coin;
-		somme = 0.; somme_carr = 0;
+		sum = 0; sum_carr = 0;
 
-		for (i = 0; i < i_coin; i++)
-		{
-			for (j = (j_coin - 1); j < size_matrix; j++)
-			{
-				if ((j - j_coin) >= i)
-				{
-					somme = somme + y[i][j];
-					somme_carr = somme_carr + pow(y[i][j], 2);
+		for (i = 0; i < i_coin; i++) {
+			for (j = (j_coin - 1); j < size_matrix; j++) {
+				if ((j - j_coin) >= i) {
+					sum = sum + y[i][j];
+					sum_carr = sum_carr + pow(y[i][j], 2);
 				}
 			}
 		}
-		mu_hat = somme / size_coin;
-		var_hat = somme_carr / (size_coin - 1) - size_coin * pow(mu_hat, 2) / (size_coin - 1);
+		mu_hat = sum / size_coin;
+		var_hat = sum_carr / (size_coin - 1) - size_coin * pow(mu_hat, 2) / (size_coin - 1);
 		phi_hat = pow(mu_hat, 2) / (var_hat - mu_hat);
 
 		/////  Calculating amounts in trapezes
-		if (((phi_hat + D[0][0]) > 0) && (D[0][0] > 0)) Delta[0][0] = -(phi_hat + D[0][0]) * log(phi_hat + D[0][0]) + D[0][0] * log(D[0][0]);
-
-		for (k = 1; k < size_matrix; k++)
-		{
+		if (((phi_hat + D[0][0]) > 0) && (D[0][0] > 0))
+			Delta[0][0] = -(phi_hat + D[0][0]) * log(phi_hat + D[0][0]) + D[0][0] * log(D[0][0]);
+		for (k = 1; k < size_matrix; k++) {
 			size_mu = ((pow(k + 1, 2) + k + 1) / 2);
 			mu = D[0][k] / size_mu;
-			if (D[0][k] > 0) Delta[0][k] = -(size_mu * phi_hat + D[0][k]) * log(phi_hat + mu) + D[0][k] * log(mu);
-			if (D[k][k] > 0) Delta[k][k] = -(phi_hat + D[k][k]) * log(phi_hat + D[k][k]) + D[k][k] * log(D[k][k]);
+			if (D[0][k] > 0)
+				Delta[0][k] = -(size_mu * phi_hat + D[0][k]) * log(phi_hat + mu) + D[0][k] * log(mu);
+			if (D[k][k] > 0)
+				Delta[k][k] = -(phi_hat + D[k][k]) * log(phi_hat + D[k][k]) + D[k][k] * log(D[k][k]);
 		}
 
-		if (strcmp(model, "Dplus") == 0)
-		{
-			for (i = 1; i < (size_matrix - 1); i++)
-			{
-				for (j = (i + 1); j < size_matrix; j++)
-				{
-					somme = 0;
-					for (k = 0; k <= i; k++) somme = somme + y[k][j];
-					T[i][j] = T[i][(j - 1)] + somme;
+		if (strcmp(model, "Dplus") == 0) {
+			for (i = 1; i < (size_matrix - 1); i++) {
+				for (j = (i + 1); j < size_matrix; j++) {
+					sum = 0;
+					for (k = 0; k <= i; k++) sum = sum + y[k][j];
+					T[i][j] = T[i][(j - 1)] + sum;
 					R[i][j] = T[(i - 1)][j] - T[(i - 1)][(i - 1)];
 					D[i][j] = T[j][j] - T[(i - 1)][j];
 					size_mat_extr = (j - i + 1) * (j - i) / 2 + (j - i + 1);
-					if ((D[i][j] > 0) && (size_mat_extr != 0))
-					{
+					if ((D[i][j] > 0) && (size_mat_extr != 0)) {
 						mu = D[i][j] / size_mat_extr;
 						Delta[i][j] = -(size_mat_extr * phi_hat + D[i][j]) * log(phi_hat + mu) + D[i][j] * log(mu);
 					}
 					size_mat_extr = i * (j - i + 1);
-					if ((R[i][j] > 0) && (size_mat_extr != 0))
-					{
+					if ((R[i][j] > 0) && (size_mat_extr != 0)) {
 						mu = R[i][j] / size_mat_extr;
 						Exterieur[i][j] = -(size_mat_extr * phi_hat + R[i][j]) * log(phi_hat + mu) + R[i][j] * log(mu);
 					}
 				}
 			}
 		}
-		else if (strcmp(model, "D") == 0)
-		{
-			for (i = 1; i < (size_matrix - 1); i++)
-			{
-				for (j = (i + 1); j < size_matrix; j++)
-				{
-					somme = 0;
-					for (k = 0; k <= i; k++) somme = somme + y[k][j];
-					T[i][j] = T[i][(j - 1)] + somme;
+		else if (strcmp(model, "D") == 0) {
+			for (i = 1; i < (size_matrix - 1); i++) {
+				for (j = (i + 1); j < size_matrix; j++) {
+					sum = 0;
+					for (k = 0; k <= i; k++) sum = sum + y[k][j];
+					T[i][j] = T[i][(j - 1)] + sum;
 					R[i][j] = T[(i - 1)][j] - T[(i - 1)][(i - 1)];
 					D[i][j] = T[j][j] - T[(i - 1)][j];
 					size_mat_extr = (j - i + 1) * (j - i) / 2 + (j - i + 1);
-					if ((D[i][j] > 0) && (size_mat_extr != 0))
-					{
+					if ((D[i][j] > 0) && (size_mat_extr != 0)) {
 						mu = D[i][j] / size_mat_extr;
 						Delta[i][j] = -(size_mat_extr * phi_hat + D[i][j]) * log(phi_hat + mu) + D[i][j] * log(mu);
 					}
 					size_mat_extr = i * (j - i + 1);
-					if (((phi_hat + mu_hat) > 0) && (mu_hat != 0))
-					{
+					if (((phi_hat + mu_hat) > 0) && (mu_hat != 0)) {
 						Exterieur[i][j] = -(size_mat_extr * phi_hat + R[i][j]) * log(phi_hat + mu_hat) + R[i][j] * log(mu_hat);
 					}
 				}
 			}
 		}
-	} else if (strcmp(distribution, "G") == 0) {
+	} else if (strcmp(distrib, "G") == 0) {
 		n_coin = (int)floor(size_matrix / 4);
 		size_coin = (n_coin + 1) * n_coin / 2;
 		j_coin = 3 * n_coin;
 		i_coin = n_coin;
-		somme = 0.;
+		sum = 0;
 
-		for (i = 0; i < i_coin; i++)
-		{
-			for (j = j_coin; j < size_matrix; j++)
-			{
-				if ((j - j_coin) >= i)
-				{
-					somme = somme + y[i][j];
+		for (i = 0; i < i_coin; i++) {
+			for (j = j_coin; j < size_matrix; j++) {
+				if ((j - j_coin) >= i) {
+					sum = sum + y[i][j];
 				}
 			}
 		}
-		mu_hat = somme / size_coin;
+		mu_hat = sum / size_coin;
 
-		/////  Calculating amounts in trapezes
+		//  Calculating amounts in trapezes
 		Tcarr[0][0] = pow(y[0][0], 2);
 		Dcarr[0][0] = pow(y[0][0], 2);
 		Delta[0][0] = 0;
 
-		for (k = 1; k < size_matrix; k++)
-		{
+		for (k = 1; k < size_matrix; k++) {
 			Tcarr[0][k] = Tcarr[0][(k - 1)] + pow(y[0][k], 2);
 			Dcarr[k][k] = pow(y[k][k], 2);
-			somme_carr = 0;
-			for (i = 0; i <= k; i++)
-			{
-				somme_carr = somme_carr + pow(y[i][k], 2);
+			sum_carr = 0;
+			for (i = 0; i <= k; i++) {
+				sum_carr = sum_carr + pow(y[i][k], 2);
 			}
-			Tcarr[k][k] = Tcarr[(k - 1)][(k - 1)] + somme_carr;
+			Tcarr[k][k] = Tcarr[(k - 1)][(k - 1)] + sum_carr;
 			Dcarr[0][k] = Tcarr[k][k];
 			mu = D[0][k] / ((pow(k + 1, 2) + k + 1) / 2);
 			Delta[0][k] = -(Dcarr[0][k] - D[0][k] * mu);
 			Delta[k][k] = 0;
 		}
-		if (strcmp(model, "Dplus") == 0)
-		{
-			for (i = 1; i < (size_matrix - 1); i++)
-			{
-				for (j = (i + 1); j < size_matrix; j++)
-				{
-					somme = 0; somme_carr = 0;
-					for (k = 0; k <= i; k++)
-					{
-						somme = somme + y[k][j];
-						somme_carr = somme_carr + pow(y[k][j], 2);
+		if (strcmp(model, "Dplus") == 0) {
+			for (i = 1; i < (size_matrix - 1); i++) {
+				for (j = (i + 1); j < size_matrix; j++) {
+					sum = 0; sum_carr = 0;
+					for (k = 0; k <= i; k++) {
+						sum = sum + y[k][j];
+						sum_carr = sum_carr + pow(y[k][j], 2);
 					}
-					T[i][j] = T[i][(j - 1)] + somme;
+					T[i][j] = T[i][(j - 1)] + sum;
 					R[i][j] = T[(i - 1)][j] - T[(i - 1)][(i - 1)];
 					D[i][j] = T[j][j] - T[(i - 1)][j];
-					Tcarr[i][j] = Tcarr[i][(j - 1)] + somme_carr;
+					Tcarr[i][j] = Tcarr[i][(j - 1)] + sum_carr;
 					Rcarr[i][j] = Tcarr[(i - 1)][j] - Tcarr[(i - 1)][(i - 1)];
 					Dcarr[i][j] = Tcarr[j][j] - Tcarr[(i - 1)][j];
 					size_mat_extr = (j - i + 1) * (j - i) / 2 + (j - i + 1);
@@ -295,22 +286,18 @@ int Function_HiC_R(int *size, int *K, char** distrib, double* matrix, int* tchap
 				}
 			}
 		}
-		else if (strcmp(model, "D") == 0)
-		{
-			for (i = 1; i < (size_matrix - 1); i++)
-			{
-				for (j = (i + 1); j < size_matrix; j++)
-				{
-					somme = 0; somme_carr = 0;
-					for (k = 0; k <= i; k++)
-					{
-						somme = somme + y[k][j];
-						somme_carr = somme_carr + pow(y[k][j], 2);
+		else if (strcmp(model, "D") == 0) {
+			for (i = 1; i < (size_matrix - 1); i++) {
+				for (j = (i + 1); j < size_matrix; j++) {
+					sum = 0; sum_carr = 0;
+					for (k = 0; k <= i; k++) {
+						sum = sum + y[k][j];
+						sum_carr = sum_carr + pow(y[k][j], 2);
 					}
-					T[i][j] = T[i][(j - 1)] + somme;
+					T[i][j] = T[i][(j - 1)] + sum;
 					R[i][j] = T[(i - 1)][j] - T[(i - 1)][(i - 1)];
 					D[i][j] = T[j][j] - T[(i - 1)][j];
-					Tcarr[i][j] = Tcarr[i][(j - 1)] + somme_carr;
+					Tcarr[i][j] = Tcarr[i][(j - 1)] + sum_carr;
 					Rcarr[i][j] = Tcarr[(i - 1)][j] - Tcarr[(i - 1)][(i - 1)];
 					Dcarr[i][j] = Tcarr[j][j] - Tcarr[(i - 1)][j];
 					size_mat_extr = (j - i + 1) * (j - i) / 2 + (j - i + 1);
@@ -328,9 +315,9 @@ int Function_HiC_R(int *size, int *K, char** distrib, double* matrix, int* tchap
 
 	/////// dynamic programming ///////////
 
-	for (i = 0; i <= Kmax - 1; i++) {
+	for (i = 0; i <= max_change_points - 1; i++) {
 		for (j = 0; j < size_matrix; j++) {
-			if(i < Kmax-1)
+			if(i < max_change_points-1)
 				t_matrix[i][j] = -1;
 			if(i!=0)
 				I[i][j] = -1E100;
@@ -339,7 +326,7 @@ int Function_HiC_R(int *size, int *K, char** distrib, double* matrix, int* tchap
 		}
 	}
 
-	for (k = 1; k <= Kmax - 1; k++) {
+	for (k = 1; k <= max_change_points - 1; k++) {
 		for (l = k; l < size_matrix; l++) {
 			for (i = 0; i < size_matrix - 1; i++) {
 				vecteur[i] = -1E100;
@@ -362,7 +349,7 @@ int Function_HiC_R(int *size, int *K, char** distrib, double* matrix, int* tchap
 
 	ind_max=0;
 	max=I[0][size_matrix-1];
-	for (i=0;i<Kmax;i++) {
+	for (i=0;i<max_change_points;i++) {
 		Jvect[i] = I[i][size_matrix-1];
 		if((I[i][size_matrix-1])>max) {
 			max=I[i][size_matrix-1];
@@ -370,8 +357,8 @@ int Function_HiC_R(int *size, int *K, char** distrib, double* matrix, int* tchap
 		}
 	}
 
-	for (i = 0; i < Kmax; i++) {
-		for (j = 0; j < Kmax; j++) {
+	for (i = 0; i < max_change_points; i++) {
+		for (j = 0; j < max_change_points; j++) {
 			if(i != j)
 				t_est[i][j] = -1;
 			else
@@ -380,18 +367,18 @@ int Function_HiC_R(int *size, int *K, char** distrib, double* matrix, int* tchap
 	}
 
 	/////// Calculating change-point /////////
-	for (j = 1; j < Kmax; j++) {
+	for (j = 1; j < max_change_points; j++) {
 		for (k = j - 1; k >= 0; k--) {
 			t_est[j][k] = t_matrix[k][(int)t_est[j][k + 1]];
 		}
 	}
 
-	//////// Calculating Kchap /////////
+	//////// Calculating max index /////////
 
-	for (i = 0; i < Kmax; i++) {
+	for (i = 0; i < max_change_points; i++) {
 		tchap[i] = (int) t_est[ind_max][i] + 1;
-		for (j = 0; j < Kmax; j++) {
-			t_est_mat[i * Kmax + j] = (int) t_est[i][j] + 1;
+		for (j = 0; j < max_change_points; j++) {
+			t_est_mat[i * max_change_points + j] = (int) t_est[i][j] + 1;
 		}
 	}
 	return (EXIT_SUCCESS);
